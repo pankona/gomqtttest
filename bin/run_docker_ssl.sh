ffbbffad65a90fcc4cee53b6e9969714b1d1678f
pankona <yosuke.akatsuka@gmail.com>
2017-06-30 18:59:17 +0900

    update docker run script

--- bin/run_docker_ssl.sh
+++ bin/run_docker_ssl.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm -t -p 8883:8883 --name mosquitto-ssl -v `pwd`/mosquitto_config/:/mosquitto/config/ -v /dev/log:/dev/log eclipse-mosquitto &
+docker run \
+    --rm \
+    -t \
+    -p 8883:8883 \
+    --name mosquitto-ssl \
+    -v `pwd`/mosquitto_config/:/mosquitto/config/ \
+    -v /dev/log:/dev/log \
+    eclipse-mosquitto &
